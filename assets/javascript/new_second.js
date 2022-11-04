481fc2ed6205d4e8ca2fa1bb8fa2dadb3599b592
LordAkirast <darkwarish@gmail.com>
2022-11-04 18:47:16 -0300

    new_second desktop page updated. JS rules implemented.

--- assets/javascript/new_second.js
+++ assets/javascript/new_second.js
@@ -1,57 +1,69 @@
-let quiz_title;
-let quiz_url;
-let quiz_question;
-let quiz_level;
-let check1 = 0;
-let check2 = 0;
-let check3 = 0;
-let check4 = 0;
+let question_text;
+let question_correct_answer;
+let question_correct_url;
+let question_incorrect_answer;
+let question_incorrect_url;
+let check1;
+let check2;
+let check3;
+let check4;
+let check5;
 
 
 
 
 function createLevels() {
-    question_text = document.querySelector('#question-text').length
-    quiz_url = document.querySelector('#quiz_url').value
-    quiz_question = document.querySelector('#quiz_question').value
-    quiz_level = document.querySelector('#quiz_level').value
+    question_text = document.querySelector('#question_text').length
+    question_correct_answer = document.querySelector('#question_correct_answer').value
+    question_correct_url = document.querySelector('#question_correct_url').value
+    question_incorrect_answer = document.querySelector('#question_incorrect_answer').value
+    question_incorrect_url = document.querySelector('#question_incorrect_url').value
 
-    if (quiz_title === '') {
-    var element = document.getElementById('quiz_title')
+    if (question_text === '') {
+    var element = document.getElementById('question_text')
     element.classList.add("wrong");
     check1 = 0;
     } else {
         check1 = 1;
     }
 
-    if (!quiz_url.includes('http') || quiz_url === '') {
-    var element = document.getElementById('quiz_url')
-    element.classList.add("wrong");
-    check2 = 0;
-    } else {
-        check2 = 1;
-    }
-
-    if ((quiz_question === '0') || (quiz_question === '')) {
-        var element = document.getElementById('quiz_question')
+    if (question_correct_answer === '') {
+        var element = document.getElementById('question_correct_answer')
         element.classList.add("wrong");
-        check3 = 0;
+        check2 = 0;
+        } else {
+            check2 = 1; 
+        }
+
+
+    
+
+    if (!question_correct_url.includes('http') || question_correct_url === '') {
+    var element = document.getElementById('question_correct_url')
+    element.classList.add("wrong");
+    check3 = 0;
     } else {
         check3 = 1;
     }
 
-
-    if (quiz_level === '') {
-        var element = document.getElementById('quiz_level')
+    if (!question_incorrect_url.includes('http') || question_incorrect_url === '') {
+        var element = document.getElementById('question_incorrect_url')
         element.classList.add("wrong");
         check4 = 0;
+        } else {
+            check4 = 1;
+        }
 
+    if ((question_incorrect_answer === '0') || (question_incorrect_answer === '')) {
+        var element = document.getElementById('question_incorrect_answer')
+        element.classList.add("wrong");
+        check5 = 0;
     } else {
-        check4 = 1;
+        check5 = 1;
     }
 
-    if(check1 + check2 + check3 + check4 === 4) {
-        window.location.replace("./new_second.html");
+    if(check1 + check2 + check3 + check4 + check5 === 5) {
+        window.location.replace("./new_third.html");
     }
 }
 
